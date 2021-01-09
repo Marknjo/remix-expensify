ac55694ca38ceb9039a7d9f0b7b562d26a5b4f1f
Mark Njoroge <marknjo7@gmail.com>
2021-01-09 08:34:51 +0300

    style auth form and import the auth component to the auth route

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,3 +1,23 @@
+import type { LinksFunction } from '@remix-run/node'
+import AuthForm from '~/components/auth/AuthForm'
+import authStyles from '~/styles/auth.css'
+
+// meta
+
+// links
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: authStyles },
+]
+
+// actions
+
+// loaders
+
+// page component
 export default function AuthPage() {
-  return <div>AuthPage</div>
+  return (
+    <main>
+      <AuthForm />
+    </main>
+  )
 }
