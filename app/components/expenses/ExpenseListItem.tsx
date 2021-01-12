0d100f9fa82890ab5417de24264d3d2433bd3854
Mark Njoroge <marknjo7@gmail.com>
2021-01-12 08:45:23 +0300

    add ExpenseListItem componet to the project

---
+++ app/components/expenses/ExpenseListItem.tsx
@@ -0,0 +1,25 @@
+interface IListItemProps {
+  title: string
+  amount: number
+}
+
+function ExpenseListItem({ title, amount }: IListItemProps) {
+  function deleteExpenseItemHandler() {
+    // tbd
+  }
+
+  return (
+    <article className="expense-item">
+      <div>
+        <h2 className="expense-title">{title}</h2>
+        <p className="expense-amount">${amount.toFixed(2)}</p>
+      </div>
+      <menu className="expense-actions">
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
+        <a href="tbd">Edit</a>
+      </menu>
+    </article>
+  )
+}
+
+export default ExpenseListItem
