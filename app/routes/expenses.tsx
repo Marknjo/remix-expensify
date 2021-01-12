b8c8b267e3fafdcd89f2001973810bfae339e570
Mark Njoroge <marknjo7@gmail.com>
2021-01-12 08:53:19 +0300

    add expenses styles to the base layout

--- app/routes/expenses.tsx
+++ app/routes/expenses.tsx
@@ -1,5 +1,19 @@
+import type { LinksFunction } from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
+import expensesStyles from '~/styles/expenses.css'
 
+// meta
+
+// links
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: expensesStyles },
+]
+
+// actions
+
+// loaders
+
+// page component
 export default function ExpensesLayout() {
   return (
     <div>
