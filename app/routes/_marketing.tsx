9a3b44d526965d92fdbdbf5731c1b71f878dc51f
Mark Njoroge <marknjo7@gmail.com>
2021-01-13 09:16:11 +0300

    add shared marketing styles

--- app/routes/_marketing.tsx
+++ app/routes/_marketing.tsx
@@ -1,4 +1,13 @@
+import type { LinksFunction } from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
+import marketingStyles from '~/styles/marketing.css'
+
+// meta
+
+// links
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: marketingStyles },
+]
 
 export default function MarketingLayout() {
   return (
