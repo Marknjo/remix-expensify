3d96b052c23010d039b09a49f39770195d7d0f2f
Mark Njoroge <marknjo7@gmail.com>
2021-01-15 09:34:20 +0300

    add layout structure of the pricing page

--- app/routes/_marketing.pricing.tsx
+++ app/routes/_marketing.pricing.tsx
@@ -1,3 +1,22 @@
+import PricingPlan from '~/components/marketing/PricingPlan'
+import { PRICING_PLANS } from '~/data'
+
 export default function PricingPage() {
-  return <div>pricing</div>
+  return (
+    <>
+      <h2>Great Product, Simple Pricing</h2>
+      <ol id="pricing-plans">
+        {PRICING_PLANS.map(plan => (
+          <li key={plan.id} className="plan">
+            <PricingPlan
+              title={plan.title}
+              price={plan.price}
+              perks={plan.perks}
+              icon={plan.icon}
+            />
+          </li>
+        ))}
+      </ol>
+    </>
+  )
 }
