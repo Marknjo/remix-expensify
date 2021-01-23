4746b37b8b36d7fdc4a34406960e05377554207e
Mark Njoroge <marknjo7@gmail.com>
2021-01-23 10:47:29 +0300

    refactor ExpenseList Component to accept Expenses without createdAt

--- app/components/expenses/ExpensesList.tsx
+++ app/components/expenses/ExpensesList.tsx
@@ -1,7 +1,11 @@
 import type { Expense } from '@prisma/client'
 import ExpenseListItem from './ExpenseListItem'
 
-function ExpensesList({ expenses }: { expenses: Expense[] }) {
+function ExpensesList({
+  expenses,
+}: {
+  expenses: Omit<Expense, 'createdAt'>[]
+}) {
   return (
     <ol id="expenses-list">
       {expenses.map(expense => (
