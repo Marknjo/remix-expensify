efaedc8f6a6d85530be04c3a701b566051481d7d
Mark Njoroge <marknjo7@gmail.com>
2021-01-23 10:48:57 +0300

    set ExpensesLayout as default page for ExpensePage & AddExpensePage

--- app/routes/_app.expenses.tsx
+++ app/routes/_app.expenses.tsx
@@ -1,4 +1,7 @@
-import { Outlet } from '@remix-run/react'
+import { Link, Outlet } from '@remix-run/react'
+import { FaDownload, FaPlus } from 'react-icons/fa'
+import ExpensesList from '~/components/expenses/ExpensesList'
+import { DUMMY_EXPENSES } from '~/data'
 
 // meta
 
@@ -8,12 +11,24 @@ import { Outlet } from '@remix-run/react'
 
 // loaders
 
-// page component
+// Layout component
 export default function ExpensesLayout() {
   return (
-    <div>
-      <h1>Base Layout</h1>
+    <>
       <Outlet />
-    </div>
+      <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
+        <ExpensesList expenses={DUMMY_EXPENSES} />
+      </main>
+    </>
   )
 }
