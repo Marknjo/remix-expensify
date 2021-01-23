045e6c190ed3cb2cb0d9b012a000d3fe5bd994b8
Mark Njoroge <marknjo7@gmail.com>
2021-01-23 10:52:16 +0300

    fix expenses styling - lacked main tag

--- app/routes/_app.expenses.analysis.tsx
+++ app/routes/_app.expenses.analysis.tsx
@@ -4,9 +4,9 @@ import { DUMMY_EXPENSES } from '~/data'
 
 export default function ExpensesAnalysisPage() {
   return (
-    <>
+    <main>
       <Chart expenses={DUMMY_EXPENSES} />
       <ExpenseStatistics expenses={DUMMY_EXPENSES} />
-    </>
+    </main>
   )
 }
