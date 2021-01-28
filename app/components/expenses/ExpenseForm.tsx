da5fc53ec69ec290e12239e38d1e1cd472664e55
Mark Njoroge <marknjo7@gmail.com>
2021-01-28 12:14:06 +0300

    prep ExpenseForm with default values to handle new and edit features

--- app/components/expenses/ExpenseForm.tsx
+++ app/components/expenses/ExpenseForm.tsx
@@ -1,13 +1,35 @@
-import { Link } from '@remix-run/react'
+import type { Expense } from '@prisma/client'
+import { Form, Link, useLoaderData } from '@remix-run/react'
 
 function ExpenseForm() {
+  const expenseData = useLoaderData<Expense>()
+
   const today = new Date().toISOString().slice(0, 10) // yields something like 2023-09-10
 
+  const defaultValues = expenseData
+    ? {
+        title: expenseData.title,
+        amount: expenseData.amount,
+        date: expenseData.date,
+      }
+    : {
+        title: '',
+        amount: '',
+        date: '',
+      }
+
   return (
-    <form method="post" className="form" id="expense-form">
+    <Form method="post" className="form" id="expense-form">
       <p>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" name="title" required maxLength={30} />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          maxLength={30}
+          defaultValue={defaultValues.title}
+        />
       </p>
 
       <div className="form-row">
@@ -20,18 +42,27 @@ function ExpenseForm() {
             min="0"
             step="0.01"
             required
+            defaultValue={defaultValues.amount}
           />
         </p>
         <p>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" max={today} required />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            max={today}
+            required
+            defaultValue={defaultValues.date}
+          />
         </p>
       </div>
+
       <div className="form-actions">
-        <button>Save Expense</button>
+        <button>'Save Expense'</button>
         <Link to="..">Cancel</Link>
       </div>
-    </form>
+    </Form>
   )
 }
 
