b44e0575cfa12fd360f72818ddff5f76e53a058b
Mark Njoroge <marknjo7@gmail.com>
2021-01-30 16:16:23 +0300

    implement loading raw data from db /expeses/raw

--- app/routes/expenses_.raw.tsx
+++ app/routes/expenses_.raw.tsx
@@ -1,3 +1,10 @@
-export default function RawExpensesPage() {
-  return <div>Raw Expenses</div>
+import type { LoaderFunction } from '@remix-run/node'
+import { getExpenses } from '~/server'
+
+export const loader: LoaderFunction = async () => {
+  try {
+    return getExpenses()
+  } catch (error) {
+    throw error
+  }
 }
