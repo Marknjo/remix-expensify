85752e929a09236f0c7bc69674209de132b92270
Mark Njoroge <marknjo7@gmail.com>
2021-02-01 18:06:43 +0300

    refactor ExpensesList to consume Expense type by default

--- app/components/expenses/ExpensesList.tsx
+++ app/components/expenses/ExpensesList.tsx
@@ -1,20 +1,12 @@
 import type { Expense } from '@prisma/client'
 import ExpenseListItem from './ExpenseListItem'
 
-function ExpensesList({
-  expenses,
-}: {
-  expenses: Omit<Expense, 'createdAt'>[]
-}) {
+function ExpensesList({ expenses }: { expenses: Expense[] }) {
   return (
     <ol id="expenses-list">
       {expenses.map(expense => (
         <li key={expense.id}>
-          <ExpenseListItem
-            id={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-          />
+          <ExpenseListItem {...expense} />
         </li>
       ))}
     </ol>
