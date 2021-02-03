38274595a06aded53c0ec232eff1d3df7c170e74
Mark Njoroge <marknjo7@gmail.com>
2021-02-03 11:55:00 +0300

    handle no user expenses in the db case jsx structure in the ExpenseList component

--- app/components/expenses/ExpensesList.tsx
+++ app/components/expenses/ExpensesList.tsx
@@ -1,7 +1,26 @@
 import type { Expense } from '@prisma/client'
 import ExpenseListItem from './ExpenseListItem'
+import { Link } from '@remix-run/react'
+import { FaPlus } from 'react-icons/fa'
 
 function ExpensesList({ expenses }: { expenses: Expense[] }) {
+  if (!expenses || expenses.length === 0) {
+    return (
+      <article className="expenses__no-expense">
+        <section>
+          <p>😟 Oops! Looks like you do not have any expenses yet!</p>
+          <p>
+            <Link to="add">
+              <div>
+                <span>Add New Expense</span> <FaPlus />
+              </div>
+            </Link>
+          </p>
+        </section>
+      </article>
+    )
+  }
+
   return (
     <ol id="expenses-list">
       {expenses.map(expense => (
