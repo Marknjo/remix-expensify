f7f7e225d3642a38555e94560b3448eb21401e4c
Mark Njoroge <marknjo7@gmail.com>
2021-02-05 12:40:17 +0300

    add auth validation errors interface IAuthValidationErrors

---
+++ app/types/IAuthValidationErrors.ts
@@ -0,0 +1,4 @@
+export default interface IAuthValidationErrors {
+  name?: string
+  email?: string
+}
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,3 +1,4 @@
 export type { default as IPricingPlan } from './IPricingPlan'
 export type { default as INewExpense } from './INewExpense'
 export type { default as INewExpenseValidationsErrors } from './INewExpenseValidationsErrors'
+export type { default as IAuthValidationErrors } from './IAuthValidationErrors'
