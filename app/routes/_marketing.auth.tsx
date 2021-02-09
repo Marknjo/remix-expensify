1b0ec856e62cdd1c3df869b0fcb944d763e2e396
Mark Njoroge <marknjo7@gmail.com>
2021-02-09 15:35:44 +0300

    implement user sign up flow by completing action method in the auth

--- app/routes/_marketing.auth.tsx
+++ app/routes/_marketing.auth.tsx
@@ -1,5 +1,7 @@
-import type { LinksFunction } from '@remix-run/node'
-import AuthForm from '~/components/auth/AuthForm'
+import type { User } from '@prisma/client'
+import { json, type ActionArgs, type LinksFunction } from '@remix-run/node'
+import AuthForm, { EAuthModes } from '~/components/auth/AuthForm'
+import { createUser, validateAuthInputs } from '~/server'
 import authStyles from '~/styles/auth.css'
 
 // meta
@@ -10,6 +12,51 @@ export const links: LinksFunction = () => [
 ]
 
 // actions
+export async function action({ request }: ActionArgs) {
+  const query = new URL(request.url).searchParams
+  const inputData = await request.formData()
+  const authInputs = Object.fromEntries(inputData)
+
+  const mode = query.get('mode')
+  const authMode = mode ?? EAuthModes.SIGN_IN
+
+  // handle sign-in
+  if (authMode === EAuthModes.SIGN_IN) {
+    // @TODO: Handle signin
+  }
+
+  // handle sign-up
+  if (authMode === EAuthModes.SIGN_UP) {
+    const newUserInputs = authInputs as unknown as Pick<
+      User,
+      'name' | 'email' | 'password'
+    >
+    // add validations
+    try {
+      validateAuthInputs(newUserInputs)
+    } catch (error) {
+      return error
+    }
+
+    try {
+      // sign-up user
+      return createUser(newUserInputs)
+    } catch (error) {
+      throw json(
+        {
+          message:
+            error instanceof Error ? error.message : 'Failed to sign up user',
+        },
+        {
+          status: 500,
+          statusText: 'Server Error',
+        },
+      )
+    }
+  }
+
+  return null
+}
 
 // loaders
 
