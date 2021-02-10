e3c497880b21101875a399ac625ccc40c626e2af
Mark Njoroge <marknjo7@gmail.com>
2021-02-10 15:53:05 +0300

    implement find signIn user api method

---
+++ app/server/api/auth/sign-in.server.ts
@@ -0,0 +1,23 @@
+import type { User } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import findUser from './find-user.server'
+import { createUserSession } from './sessions-utils.server'
+
+export default async function signIn(input: Pick<User, 'email' | 'password'>) {
+  try {
+    const plainPassword = input.password
+    const email = input.email
+
+    const user = await findUser(email)
+
+    const hashedPassword = user.password
+
+    if (!(await bcrypt.compare(plainPassword, hashedPassword))) {
+      throw new Error('Invalid password')
+    }
+
+    createUserSession(user.id, '/expenses')
+  } catch (error) {
+    throw 'Username or Password invalid'
+  }
+}
