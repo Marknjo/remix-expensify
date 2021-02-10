a2063dffe2a1fa2c6ff2c09605a01bd8bb4310f6
Mark Njoroge <marknjo7@gmail.com>
2021-02-10 16:25:35 +0300

    fix validation implementation by addign whitelisting of non required fields to validate

--- app/server/validations/new-user/auth.validations.server.ts
+++ app/server/validations/new-user/auth.validations.server.ts
@@ -1,6 +1,14 @@
 import type { User } from '@prisma/client'
 import type { IAuthValidationErrors } from '~/types'
 
+export enum EAuthFields {
+  NAME = 'NAME',
+  EMAIL = 'EMAIL',
+  PASSWORD = 'PASSWORD',
+}
+
+export type TAuthIgnoreList = Set<keyof typeof EAuthFields>
+
 function isUsername(value?: string) {
   return value && value.trim().length > 0 && value.trim().length <= 50
 }
@@ -13,18 +21,24 @@ function isValidPassword(value?: string) {
   return value && value.trim().length >= 7
 }
 
-export default function validateCredentials(input: Partial<User>) {
+export default function validateCredentials(
+  input: Partial<User>,
+  ignoreList?: TAuthIgnoreList,
+) {
   let validationErrors: IAuthValidationErrors = {}
 
-  if (!isUsername(input?.name)) {
+  if (!ignoreList?.has(EAuthFields.NAME) && !isUsername(input?.name)) {
     validationErrors.name = 'Invalid username format.'
   }
 
-  if (!isValidEmail(input?.email)) {
+  if (!ignoreList?.has(EAuthFields.EMAIL) && !isValidEmail(input?.email)) {
     validationErrors.email = 'Invalid email address.'
   }
 
-  if (!isValidPassword(input?.password)) {
+  if (
+    !ignoreList?.has(EAuthFields.PASSWORD) &&
+    !isValidPassword(input?.password)
+  ) {
     validationErrors.password =
       'Invalid password. Must be at least 7 characters long.'
   }
