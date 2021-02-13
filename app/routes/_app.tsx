7cedccc27b846d051f0d3ac3a6142cfe9112ed7d
Mark Njoroge <marknjo7@gmail.com>
2021-02-13 16:53:32 +0300

    use isLoggedIn guard to protect /expenses parent route

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,6 +1,7 @@
-import type { LinksFunction } from '@remix-run/node'
+import { redirect, type LinksFunction, type LoaderArgs } from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
 import ExpensesHeader from '~/components/navigation/ExpensesHeader'
+import { isLoggedIn } from '~/server'
 
 import expensesStyles from '~/styles/expenses.css'
 
@@ -9,6 +10,17 @@ import expensesStyles from '~/styles/expenses.css'
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: expensesStyles },
 ]
+
+// loader
+export async function loader({ request }: LoaderArgs) {
+  const userId = await isLoggedIn(request)
+
+  if (userId) {
+    return null
+  }
+
+  return redirect('/auth')
+}
 
 // layout
 export default function AppLayout() {
