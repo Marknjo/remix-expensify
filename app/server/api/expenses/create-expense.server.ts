7b2d91eac68a3a1403f9d17061c86d7094df1d32
Mark Njoroge <marknjo7@gmail.com>
2021-02-17 08:11:22 +0300

    associate a new expense with a current logged in user

--- app/server/api/expenses/create-expense.server.ts
+++ app/server/api/expenses/create-expense.server.ts
@@ -1,15 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions, @typescript-eslint/no-unused-vars */
+import type { ActionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import type { INewExpense } from '~/types'
 import { db } from '~/utils/db.server'
+import isLoggedIn from '../auth/is-logged-in.server'
 
-export default async function createExpense(newExpense: INewExpense) {
+export default async function createExpense(
+  newExpense: INewExpense,
+  request: ActionArgs['request'],
+) {
   try {
+    const loggedInUser = await isLoggedIn(request)
+
     await db.expense.create({
       data: {
         title: newExpense.title,
         amount: +newExpense.amount,
         date: new Date(newExpense.date),
+        userId: loggedInUser,
 
         /// @TODO: use user settings to se currency details
         locale: 'en-KE',
