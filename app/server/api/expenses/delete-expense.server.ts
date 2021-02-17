7dc06314d146827d42cf03034b47863ac9f0daab
Mark Njoroge <marknjo7@gmail.com>
2021-02-17 08:10:21 +0300

    delete expense based on the loggedIn user

--- app/server/api/expenses/delete-expense.server.ts
+++ app/server/api/expenses/delete-expense.server.ts
@@ -1,8 +1,8 @@
 import { db } from '~/utils/db.server'
 
-export default async function deleteExpense(id: string) {
+export default async function deleteExpense(id: string, userId: string) {
   try {
-    await db.expense.delete({ where: { id } })
+    await db.expense.delete({ where: { id, userId } })
   } catch (error) {
     throw new Error(`Failed to delete expense with the id ${id}`)
   }
